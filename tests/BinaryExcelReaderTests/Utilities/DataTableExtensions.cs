b94b559fe2e1a11670409c0289dd459cc18ba3cc
Ninjanaut <lukas.prn@email.cz>
2021-09-04 23:26:09 +0200

    Initial release

---
+++ tests/BinaryExcelReaderTests/Utilities/DataTableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace BinaryExcelReaderTests.Utilities
+{
+    public static class DataTableExtensions
+    {
+        public static void AddRow(this DataTable datatable, object[] rowData)
+        {
+            var row = datatable.NewRow();
+            row.ItemArray = rowData;
+            datatable.Rows.Add(row);
+        }
+    }
+}
